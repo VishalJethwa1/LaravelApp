2d5128a2bc9e5f73b54a32a6c33275b41ee00952
--replace-all <jethwa.vishal88@gmail.com>
2024-07-16 13:06:23 +0500

    project setup completed

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -17,7 +17,7 @@
         </ul>
     </nav> -->
 
-    <div class="w-full flex flex-wrap p-3 flex-col items-end shadow-lg bg-gray-100">
+    <div class="w-full flex flex-wrap p-3 flex-col items-end shadow-lg bg-stone-700 text-stone-300">
      <div class="flex flex-wrap items-center text-base justify-center uppercase mr-20">
        <a href="" class="mr-5">home</a>
        <a href="" class="mr-5">products</a>
@@ -28,9 +28,23 @@
     </div>
     @show
 
-    @section('footer')
-    <footer class="">
+    @section('content')
+    <div class="container mx-auto p-5 bg-stone-100 h-screen ">
+        
+    </div>
+    @show
 
+
+    @section('footer')
+    <footer class="w-full p-5 columns-6 bg-stone-800 h-40 text-stone-400 text-center">
+        <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>Fourth</li>
+            <li>Fifth</li>
+            <li>Sixth</li>
+        </ul>
     </footer>
     @show
 
