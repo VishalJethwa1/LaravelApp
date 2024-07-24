d761ea9f5a741048a7ffff6e9811e62dc30cc2b2
--replace-all <jethwa.vishal88@gmail.com>
2024-07-24 16:27:16 +0500

    sign in section of laravel breeze successfully cusotmized

--- resources/views/auth/myregister.blade.php
+++ resources/views/auth/myregister.blade.php
@@ -10,9 +10,15 @@
 </head>
 <body>
 
-    <div class="container mx-auto p-60">
+    <div class="container mx-auto px-60">
 
-    <form>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up for your account</h2>
+  </div>
+
+    <form class="pt-20">
   <div class="space-y-12">
     <div class="border-b border-gray-900/10 pb-12">
       <h2 class="text-base font-semibold leading-7 text-gray-900">Profile</h2>
--- resources/views/mywelcome.blade.php
+++ resources/views/mywelcome.blade.php
@@ -19,6 +19,35 @@
                                     >
                                         Dashboard
                                     </a>
+
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Home
+                                    </a>
+
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Products
+                                    </a>
+
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        About
+                                    </a>
+
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Contact Us
+                                    </a>
+
                                 @else
                                     <a
                                         href="{{ route('login') }}"
